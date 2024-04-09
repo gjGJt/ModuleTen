5cf3b261ff9d2d404d214fd7bc8e13fd405748f3
gjGJt <157390876+gjGJt@users.noreply.github.com>
2024-04-09 18:48:32 -0500

    Update TENTWO.cpp

--- TENTWO.cpp
+++ TENTWO.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+//recursive function
+int fib(int x) {
+   if((x==1)||(x==0)) {
+      return(x);
+   }else {
+      return(fib(x-1)+fib(x-2));
+   }
+}
+
+
+int main() {
+   int x , i=0;
+   //user input
+   cout << "Enter the number of terms: ";
+   cin >> x;
+   cout << "Fibonnaci Series : ";
+   //output
+   while(i < x) {
+      cout << " " << fib(i);
+      i++;
+   }
+   return 0;
+}
+
 
