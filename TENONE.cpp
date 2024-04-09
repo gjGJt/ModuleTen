369e30a2ee11abd47f0f858a1ac3a8681960163b
gjGJt <157390876+gjGJt@users.noreply.github.com>
2024-04-09 18:45:27 -0500

    Update TENONE.cpp

--- TENONE.cpp
+++ TENONE.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial (int);
+
+int main ()
+{
+    //declaring var
+    int fact;
+    int factorialNum;
+    cout << "Enter a number to determine it's factorial: ";
+    cin >> fact;
+    fact = factorial (fact);
+
+    //output
+    cout << "Factorial is: " << fact << endl;
+    
+       return 0;
+
+
+}
+
+//recursive method for factorial
+int factorial (int n)
+{
+    if (n==0)
+    {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
 
